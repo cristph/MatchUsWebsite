da29ffe0e5721dccf6f7830389a169b0ebe137e9
wanghao <928018425@qq.com>
2015-07-10 17:30:27 +0800

    新增了User相关操作的类

---
+++ src/main/java/us/match/website/service/serviceImpl/FilterServiceImpl.java
@@ -0,0 +1,12 @@
+package us.match.website.service.serviceImpl;
+
+import org.springframework.stereotype.Service;
+import us.match.website.service.FilterService;
+
+/**
+ * Created by WH on 2015/7/10.
+ */
+@Service("filterServiceImpl")
+public class FilterServiceImpl implements FilterService{
+
+}
