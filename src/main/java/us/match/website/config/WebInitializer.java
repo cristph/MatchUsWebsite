cd87fa84fc8c19e6dc5ac09841953b938b61565f
ZhengzhongZhang <zhengzhongzhang@outlook.com>
2015-07-10 15:53:30 +0800

    Chang from xml to javaConfig

---
+++ src/main/java/us/match/website/config/WebInitializer.java
@@ -0,0 +1,26 @@
+package us.match.website.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by Ezio on 7/10/2015.
+ */
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(AppConfig.class);
+
+        rootContext.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
