83ee2f1cf00acb4defb230c2ac896384b1324d69
Seven <chinaqih@163.com>
2015-07-11 15:16:37 +0800

    -

--- src/main/java/us/match/website/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/us/match/website/service/serviceImpl/UserServiceImpl.java
@@ -9,19 +9,18 @@ import us.match.website.service.UserService;
 import javax.annotation.Resource;
 
 
-
 /**
  * Created by WH on 2015/7/10.
  */
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     @Resource
     UserDao userDao;
 
     @Override
     public String login(String username, String password) {
-        User user=new User();
+        User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         return "success";
@@ -29,7 +28,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public String logout(String username) {
-        User user=new User();
+        User user = new User();
         user.setUsername(username);
         return "success";
     }
