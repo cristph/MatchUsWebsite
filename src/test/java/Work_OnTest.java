4cd80bf514684c579d9ed0bd535572ba79625389
zhouyao <465033064@qq.com>
2015-07-11 16:36:49 +0800

    work on test

---
+++ src/test/java/Work_OnTest.java
@@ -0,0 +1,56 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import us.match.website.dao.UserDao;
+import us.match.website.dao.Work_OnDao;
+import us.match.website.model.User;
+import us.match.website.model.Work_On;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by apple on 2015/7/11.
+ */
+public class Work_OnTest {
+    static{
+        File f=new File(".");
+        System.out.println(f.getAbsolutePath());
+    }
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    Work_OnDao wo=(Work_OnDao)context.getBean("Work_OnDaoImpl");
+
+    Work_On w=new Work_On();
+    @Before
+    public void setUp(){
+        try {
+            w.setUid("wh");
+            w.setNow(true);
+            w.setWid(0);
+            w.setPast(false);
+            w.setWill(true);
+            w.setPid(1);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(true, wo.addWO(w));
+    }
+
+//    @Test(timeout=1000)
+//    public void testDelete(){
+//        assertEquals(true,ud.deleteUser(u));
+//    }
+
+//    @Ignore("Update() Not yet implemented")
+//    @Test(timeout=1000)
+//
+//    public void testUpdate(){
+//
+//    }
+}
