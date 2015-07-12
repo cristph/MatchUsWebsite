b97f0d2ac41a5f26f2afbc5c50f3d1ef64ff0cff
zhouyao <465033064@qq.com>
2015-07-12 09:24:16 +0800

    MD5 OK

--- src/main/java/us/match/website/model/User.java
+++ src/main/java/us/match/website/model/User.java
@@ -56,28 +56,7 @@ public class User {
         return password;
     }
     public void setPassword(String password) {
-        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9'
-                ,'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
-                'O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-                'o','p','q','r','s','t','u','v','w','x','y','z'};
-        try {
-            byte[] btInput = password.getBytes();
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-            mdInst.update(btInput);
-            byte[] md = mdInst.digest();
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
-            }
-            this.password=new String(str);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        this.password=password;
     }
 
     public void setUsername(String username) {
