97039b84e0683ecebb94ce4ad514e0fecc9939aa
wanghao <928018425@qq.com>
2015-07-12 15:25:46 +0800

    提交了验证码校验

--- src/main/java/us/match/website/controller/CheckCodeController.java
+++ src/main/java/us/match/website/controller/CheckCodeController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import us.match.website.service.serviceImpl.IdentifyingCode;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -21,11 +23,14 @@ import java.io.IOException;
 @Controller
 public class CheckCodeController {
     @RequestMapping(value="/checkCode.jpg")
-    public void getCheckCode(HttpServletResponse response,Model model){
+    public void getCheckCode(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Model model){
         //设置不缓存图片
-        response.setHeader("Pragma", "No-cache");
-        response.setHeader("Cache-Control", "No-cache");
+        response.setHeader("pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0) ;
+
         //指定生成的相应图片
         response.setContentType("image/jpeg") ;
         IdentifyingCode idCode = new IdentifyingCode();
@@ -44,13 +49,28 @@ public class CheckCodeController {
         idCode.drawRandomLines(g, 30) ;
         String codeString=idCode.drawRandomString(4, g) ;
         g.dispose() ;
+        request.getSession(true).setAttribute("checkCode", codeString);
         try {
             ImageIO.write(image, "JPEG", response.getOutputStream()) ;
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("输出验证码失败！");
         }
-        model.addAttribute("codeString",codeString);
+    }
+
+    @RequestMapping(value="/validate")
+    public boolean validate(@RequestParam("userCheckCode") String code,
+                            HttpServletRequest request){
+        HttpSession session=request.getSession(false);
+        if(session == null){
+            return false;
+        }
+        String checkCode=(String) session.getAttribute("checkCode");
+        session.removeAttribute("checkCode");
+        if(code!=null && code.length()>0 && code.toUpperCase().equals(checkCode) ){
+            return true;
+        }else
+            return false;
     }
 
 
