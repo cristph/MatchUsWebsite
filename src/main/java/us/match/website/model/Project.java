e731854784894225200a7fae592e494f096bd6ba
zhouyao <465033064@qq.com>
2015-07-13 13:11:02 +0800

    change database

--- src/main/java/us/match/website/model/Project.java
+++ src/main/java/us/match/website/model/Project.java
@@ -1,34 +1,26 @@
 package us.match.website.model;
 
 import javax.annotation.Generated;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by apple on 2015/7/9.
  */
 @Entity
 public class Project {
-    private String id="";
+    private String pid="";
     private String pinstruction="";
     private String pname="";
-    private String publisherid="";
     private byte[] picture;
     private boolean state;
     private String subject;
     private String moduel;
     private String skill;
     private String reward;
-    @Id
-    @GeneratedValue
-    public String getId() {
-        return id;
-    }
+    private User publisher;
+    private Set<User> workers;
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getPinstruction() {
         return pinstruction;
@@ -45,15 +37,6 @@ public class Project {
     public void setPname(String pname) {
         this.pname = pname;
     }
-
-    public String getPublisherid() {
-        return publisherid;
-    }
-
-    public void setPublisherid(String publisherid) {
-        this.publisherid = publisherid;
-    }
-
 
     public byte[] getPicture() {
         return picture;
@@ -102,5 +85,37 @@ public class Project {
 
     public void setReward(String reward) {
         this.reward = reward;
+    }
+
+    @Id
+    @GeneratedValue
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="uid")
+    public User getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(User publisher) {
+        this.publisher = publisher;
+    }
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="u_p",
+            joinColumns= @JoinColumn(name="project_id"),
+            inverseJoinColumns=@JoinColumn(name="user_id")
+    )
+    public Set<User> getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(Set<User> workers) {
+        this.workers = workers;
     }
 }
