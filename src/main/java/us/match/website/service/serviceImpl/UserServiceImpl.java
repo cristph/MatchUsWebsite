151f93f7774627a23c1840b0cf730eadf1933d04
wanghao <928018425@qq.com>
2015-07-14 13:13:10 +0800

    更新用户资料

--- src/main/java/us/match/website/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/us/match/website/service/serviceImpl/UserServiceImpl.java
@@ -69,5 +69,51 @@ public class UserServiceImpl implements UserService {
         return null;
     }
 
+    @Override
+    public User editUserInfo(User user) {
+        if(user==null)
+            return null;
+        User OutdatedUser=null;
+        /*根据email查找用户*/
+        if(user.getUsername()!=null)
+            OutdatedUser=userDao.findUser(user.getEmail());
+        if(OutdatedUser==null)
+            return null;
+        /*OutDatedUser不为null*/
+        /*更新个人简介*/
+        if(!OutdatedUser.getInstruction().equals(user.getInstruction())
+                && user.getInstruction()!=null)
+            OutdatedUser.setInstruction(user.getInstruction());
+        /*更新生日*/
+        if(!OutdatedUser.getBirthday().equals(user.getBirthday())
+                && user.getBirthday()!=null)
+            OutdatedUser.setBirthday(user.getBirthday());
+        /*更新手机*/
+        if(!OutdatedUser.getMobilephone().equals(user.getMobilephone())
+                && user.getMobilephone()!=null)
+            OutdatedUser.setMobilephone(user.getMobilephone());
+        /*更新Qq*/
+        if(!OutdatedUser.getQq().equals(user.getQq())
+                && user.getQq()!=null)
+            OutdatedUser.setQq(user.getQq());
+        /*更新大学*/
+        if(!OutdatedUser.getUniversity().equals(user.getUniversity())
+                && user.getUniversity()!=null)
+            OutdatedUser.setUniversity(user.getUniversity());
+        /*更新专业*/
+        if(!OutdatedUser.getMajor().equals(user.getMajor())
+                && user.getMajor()!=null)
+            OutdatedUser.setMajor(user.getMajor());
+        /*更新擅长*/
+        if(!OutdatedUser.getGoodat().equals(user.getGoodat())
+                && user.getGoodat()!=null)
+            OutdatedUser.setGoodat(user.getGoodat());
+        boolean  res=userDao.updateUser(OutdatedUser);
+        if(res)
+            return OutdatedUser;
+        else
+            return null;
+    }
+
 
 }
