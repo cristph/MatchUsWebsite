6e2eeb542fafd5554ecfa85b45b4a7634403e864
ZhengzhongZhang <zhengzhongzhang@outlook.com>
2015-07-15 20:59:06 +0800

    add SpringHttpMessageConverter, maybe  work :D

--- src/main/java/us/match/website/config/MvcConfig.java
+++ src/main/java/us/match/website/config/MvcConfig.java
@@ -3,13 +3,19 @@ package us.match.website.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Ezio on 7/10/2015.
@@ -30,5 +36,15 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(
+                Arrays.asList(
+                        new MediaType("text", "html", Charset.forName("UTF-8")),
+                        new MediaType("application", "json", Charset.forName("UTF-8"))));
+        converters.add(stringConverter);
     }
 }
