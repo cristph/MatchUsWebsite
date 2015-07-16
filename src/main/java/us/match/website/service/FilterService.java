f7775b6bdebe9976fb992c90c887171285c74e50
wanghao <928018425@qq.com>
2015-07-16 16:33:02 +0800

    no message

--- src/main/java/us/match/website/service/FilterService.java
+++ src/main/java/us/match/website/service/FilterService.java
@@ -3,11 +3,12 @@ package us.match.website.service;
 import us.match.website.model.Project;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by WH on 2015/7/10.
  */
 public interface FilterService {
     /*过滤接口*/
-    public ArrayList<Project> filter(ArrayList<String> keyWords);
+    public List<Project> filter(ArrayList<String> keyWords);
 }
