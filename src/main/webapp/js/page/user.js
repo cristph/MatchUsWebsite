37ffb028cd743882696a66b9fbf8c302feddf295
B_O_B <1248373163@qq.com>
2015-07-16 20:41:24 +0800

    修改安卓iOS对应错误

--- src/main/webapp/js/page/user.js
+++ src/main/webapp/js/page/user.js
@@ -1,5 +1,20 @@
-var changeBar = function () {
+var changeBar = function (uid) {
     changeBarClass(this);
+    hideAllContents();
+    switch (this.id) {
+        case "publish":
+            getPublish_unCompleted(uid);
+            break;
+        case "attend":
+            getAttend_unCompleted(uid);
+            break;
+        case "follow":
+            getFollow(uid);
+            break;
+        case "fans":
+            getFans(uid);
+            break;
+    }
 }
 
 function changeBarClass(bar) {
@@ -11,4 +26,34 @@ function changeBarClass(bar) {
         }
     }
     bar.classList.add("active");
+}
+
+function hideAllContents() {
+    var divs = document.getElementsByClassName("content_div");
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].classList.contains("show")) {
+            divs[i].classList.remove("show");
+            divs[i].classList.add("hide");
+        }
+    }
+}
+
+function getPublish_unCompleted(uid) {
+    var attent_projects = document.getElementById("attent_project");
+    alert(uid);
+    //$.post("url", uid, function (data) {
+    //
+    //});
+}
+
+function getAttend_unCompleted(uid) {
+    alert(uid);
+}
+
+function getFollow(uid) {
+    alert(uid);
+}
+
+function getFans(uid) {
+    alert(uid);
 }
