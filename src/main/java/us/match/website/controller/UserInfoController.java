9d41102f47913f78fb7019a0325382a9c3f91ea6
wanghao <928018425@qq.com>
2015-07-16 11:45:19 +0800

    no message

--- src/main/java/us/match/website/controller/UserInfoController.java
+++ src/main/java/us/match/website/controller/UserInfoController.java
@@ -1,6 +1,7 @@
 package us.match.website.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,12 +36,10 @@ public class UserInfoController extends MultiActionController{
     UserService userService;
 
     @RequestMapping(value="/user")
-    public User getBasicInfo(HttpServletRequest req,
-                                     HttpServletResponse res){
-        String name=req.getParameter("UserName");
-        String pass=req.getParameter("Password");
-        User user=userService.login(name,pass);
-        return user;
+    public String getBasicInfo(HttpSession session,Model model){
+        User u=(User)session.getAttribute("user");
+        model.addAttribute("user",u);
+        return "user/user";
     }
 
     @RequestMapping(value="/userPhoto.jpg")
