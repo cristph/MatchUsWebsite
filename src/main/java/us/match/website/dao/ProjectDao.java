6710e7da3c83e99d27215eba39c7088d8bc70234
zhouyao <465033064@qq.com>
2015-07-17 10:20:54 +0800

    change projectdaoimpl

--- src/main/java/us/match/website/dao/ProjectDao.java
+++ src/main/java/us/match/website/dao/ProjectDao.java
@@ -13,12 +13,13 @@ public interface ProjectDao {
     public Project getbyid(int id);
     public Project getbyname(String pname);
     public int     getidbyname(String name);
-    public List<Integer> getbysubject(String[] subject);
-    public List<Integer>  getbymodule(String[] module);
-    public List<Integer>  getbyskill(String[] skill);
-    public List<Integer>  getbyreward(int maxmoney,int minmoney);
+    public List<Project> getbysubject(String[] subject);
+    public List<Project>  getbymodule(String[] module);
+    public List<Project>  getbyskill(String[] skill);
+    public List<Project>  getbyreward(int maxmoney,int minmoney);
     public boolean addProject(Project project,User user);
     public boolean deleteProject(Project projetc);
     public boolean updateProject(Project project);
     public List<Project>  getallProject();
+    public List<Project>  findProject(String keyword);
 }
