64cc63f54eb31607cdeacc97adf3185f2f2d3b75
Seven <chinaqih@163.com>
2015-07-17 14:57:27 +0800

    jsp

--- src/main/webapp/js/page/releasepro.js
+++ src/main/webapp/js/page/releasepro.js
@@ -41,3 +41,37 @@ $(document).ready(function(){
         $(".hidefaxi").toggle("100");
     });
 });
+
+
+$(function(){
+    $('#submit_btn').click(
+        function(){
+                $.post("/validate.do",
+                    {"projectname":document.getElementById('projectname').value,
+                        "projectdetail":document.getElementById('projectdetail').value,
+                        "projecttag":document.getElementById('projecttag').value,
+                        "protech":document.getElementById('protech').value,
+                        "proreward":document.getElementById('proreward').value,
+                        "protech":document.getElementById('protech').value,
+                        "mobilephone":document.getElementById('mobilephone').value,
+                        "qq":document.getElementById('qq').value,
+                        "email":document.getElementById('email').value,
+                        "phone":document.getElementById('phone').value,
+                        "fax":document.getElementById('fax').value
+                    },
+                    function(data){
+                        //alert(data);
+                        if (data=="true") {
+                            window.top.location.href='/';
+                        } else if(data=="RegisterFalse"){
+                            alert('用户名已存在，注册失败！');
+                        }
+                        else {
+                            myReload();
+                            alert('验证码错误！');
+                        }
+                    });
+
+        }
+    );
+});
