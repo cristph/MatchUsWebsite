91e63f0bac32d73036c8af554620755b322ed19c
wanghao <928018425@qq.com>
2015-07-18 15:14:05 +0800

    modify interfaces

--- src/main/java/us/match/website/controller/UploadController.java
+++ src/main/java/us/match/website/controller/UploadController.java
@@ -17,41 +17,41 @@ import java.util.Iterator;
  */
 public class UploadController {
 
-//    @ResponseBody
-//    @RequestMapping(value="",method = RequestMethod.POST)
-//    public String handleRequest(HttpServletRequest request)
-//            throws IllegalStateException, IOException {
-//        CommonsMultipartResolver multipartResolver = new
-//                CommonsMultipartResolver(request.getSession().getServletContext());
-//        if(multipartResolver.isMultipart(request)){
-//            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;
-//            Iterator<String> iter = multiRequest.getFileNames();
-//            while(iter.hasNext()){
-//                int pre = (int) System.currentTimeMillis();
-//                MultipartFile file = multiRequest.getFile(iter.next());
-//                if(file != null){
-//                    String myFileName = file.getOriginalFilename();
-//                    if(myFileName.trim() !=""){
-//                        System.out.println(myFileName);
-//                        String fileName = new Date().getTime() + file.getOriginalFilename();
-//                        String path = "H:/" + fileName;
-//                        File localFile = new File(path);
-//                        file.transferTo(localFile);
-//                    }
-//                }
-//                //记录上传该文件后的时间
-//                int finaltime = (int) System.currentTimeMillis();
-//                System.out.println(finaltime - pre);
-//            }
-//
-//        }
-//        return "/success";
-//
-//    }
+    @ResponseBody
+    @RequestMapping(value="/upload",method = RequestMethod.POST)
+    public String handleRequest(HttpServletRequest request)
+            throws IllegalStateException, IOException {
+        CommonsMultipartResolver multipartResolver = new
+                CommonsMultipartResolver(request.getSession().getServletContext());
+        if(multipartResolver.isMultipart(request)){
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;
+            Iterator<String> iter = multiRequest.getFileNames();
+            while(iter.hasNext()){
+                int pre = (int) System.currentTimeMillis();
+                MultipartFile file = multiRequest.getFile(iter.next());
+                if(file != null){
+                    String myFileName = file.getOriginalFilename();
+                    if(myFileName.trim() !=""){
+                        System.out.println(myFileName);
+                        String fileName = new Date().getTime() + file.getOriginalFilename();
+                        String path =getFilePath(myFileName);
+                        File localFile = new File(path);
+                        file.transferTo(localFile);
+                    }
+                }
+                //记录上传该文件后的时间
+                int finaltime = (int) System.currentTimeMillis();
+                System.out.println(finaltime - pre);
+            }
+
+        }
+        return "/success";
+
+    }
 
     public String getFilePath(String fileName){
         File file=new File(System.getProperty("user.dir"));
-        return file.getAbsolutePath()+"target/MatchUsWebsite/image";
+        return file.getAbsolutePath()+"/target/MatchUsWebsite/image/projectImage"+fileName;
     }
 
     public static void main(String[] args) {
