1fe1fea316681e29484b28ff51e43bccf6bfb6a5
B_O_B <1248373163@qq.com>
2015-07-18 16:03:13 +0800

    加loading效果图

--- src/main/webapp/js/page/project.js
+++ src/main/webapp/js/page/project.js
@@ -19,10 +19,11 @@ function applySelect() {
     for (var i = 0; i < primary_btn_list.length; i++) {
         dataContent[i] = primary_btn_list[i].name;
     }
+    addcloud();
     $.post("/Category", {"dataContent": dataContent}, function (projects) {
         refreshView(projects);
     });
-
+    setTimeout(removecloud,300);
 }
 
 function refreshView(projects) {
@@ -114,3 +115,34 @@ function changeToPrimary(btn) {
     }
     btn.classList.add("btn-primary");
 }//将按钮变为选中
+
+function addcloud() {
+    var bodyWidth = document.documentElement.clientWidth;
+    var bodyHeight = Math.max(document.documentElement.clientHeight, document.body.scrollHeight);
+    var bgObj = document.createElement("div" );
+    bgObj.setAttribute( 'id', 'bgDiv' );
+    bgObj.style.position = "absolute";
+    bgObj.style.top = "0";
+    bgObj.style.background = "#000000";
+    bgObj.style.filter = "progid:DXImageTransform.Microsoft.Alpha(style=3,opacity=25,finishOpacity=75" ;
+    bgObj.style.opacity = "0.5";
+    bgObj.style.left = "0";
+    bgObj.style.width = bodyWidth + "px";
+    bgObj.style.height = bodyHeight + "px";
+    bgObj.style.zIndex = "10000"; //设置它的zindex属性，让这个div在z轴最大，用户点击页面任何东西都不会有反应|
+    document.body.appendChild(bgObj); //添加遮罩
+    var loadingObj = document.createElement("div");
+    loadingObj.setAttribute( 'id', 'loadingDiv' );
+    loadingObj.style.position = "absolute";
+    loadingObj.style.top =  "200px";
+    loadingObj.style.left = bodyWidth / 11*5 + "px";
+    loadingObj.style.background = "url(/image/base/loading.gif)" ;
+    loadingObj.style.width = "100px";
+    loadingObj.style.height = "100px";
+    loadingObj.style.zIndex = "10000";
+    document.body.appendChild(loadingObj); //添加loading动画-
+}
+function removecloud() {
+    $( "#loadingDiv").remove();
+    $( "#bgDiv").remove();
+}
