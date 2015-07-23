e2af6bba8cdc0f8d1339183bed3c32d8213be3f5
wanghao <928018425@qq.com>
2015-07-23 14:23:34 +0800

    edit infodao

--- src/main/java/us/match/website/dao/InfoDao.java
+++ src/main/java/us/match/website/dao/InfoDao.java
@@ -9,8 +9,8 @@ import java.util.List;
  * Created by apple on 2015/7/23.
  */
 public interface InfoDao {
-    public List<Information> getreadinfobyid(int uid);
-    public List<Information> getreadedinfobyid(int uid);
+    public List<Information> getReadInfoById(int uid);
+    public List<Information> getNotReadInfoById(int uid);
     public List<Information> getsendinfobyid(int uid);
     public boolean addinfo(Information info,User sender,User receiver);
     public boolean deleteinfo(Information info,User user);
