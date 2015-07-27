7ab0b99d76641ed7ccfe3a350fa9ed9759619b0d
cristph <1174887809@qq.com>
2015-07-27 15:04:05 +0800

    add searchController and searchService

--- src/main/java/us/match/website/controller/SearchController.java
+++ src/main/java/us/match/website/controller/SearchController.java
@@ -1,7 +1,14 @@
 package us.match.website.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by cristph on 2015/7/27.
  */
+
+@Controller
+@RequestMapping("/search")
 public class SearchController {
+
 }
