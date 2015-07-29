b8512ad67608de379c8106b6717039aea93dc47a
zhouyao <465033064@qq.com>
2015-07-29 11:45:20 +0800

    implement postservice

--- src/main/java/us/match/website/service/serviceImpl/PostServiceImpl.java
+++ src/main/java/us/match/website/service/serviceImpl/PostServiceImpl.java
@@ -2,6 +2,7 @@ package us.match.website.service.serviceImpl;
 
 import org.springframework.stereotype.Service;
 import us.match.website.dao.PostDao;
+import us.match.website.dao.ReplyDao;
 import us.match.website.model.Post;
 import us.match.website.model.Reply;
 import us.match.website.service.PostService;
@@ -17,6 +18,8 @@ public class PostServiceImpl implements PostService{
 
     @Resource
     PostDao postDao;
+    @Resource
+    ReplyDao replyDao;
 
     @Override
     public void addPost(Post post) {
@@ -25,16 +28,16 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public List<Post> getAllPost(int pid) {
-        return null;
+       return postDao.getpostByProject(pid);
     }
 
     @Override
     public void addReply(Reply reply) {
-
+        replyDao.AddReply(reply);
     }
 
     @Override
     public List<Reply> getAllReply(int postID) {
-        return null;
+        return replyDao.getReplybyPost(postID);
     }
 }
