1686c3d4bc1f195bc15b537a19fc27492a0a96ca
cristph <1174887809@qq.com>
2015-07-29 11:06:47 +0800

    update

--- src/main/java/us/match/website/service/serviceImpl/PostServiceImpl.java
+++ src/main/java/us/match/website/service/serviceImpl/PostServiceImpl.java
@@ -3,6 +3,7 @@ package us.match.website.service.serviceImpl;
 import org.springframework.stereotype.Service;
 import us.match.website.dao.PostDao;
 import us.match.website.model.Post;
+import us.match.website.model.Reply;
 import us.match.website.service.PostService;
 
 import javax.annotation.Resource;
@@ -24,6 +25,16 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public List<Post> getAllPost(int pid) {
+        return null;
+    }
+
+    @Override
+    public void addReply(Reply reply) {
+
+    }
+
+    @Override
+    public List<Reply> getAllReply(int postID) {
         return null;
     }
 }
